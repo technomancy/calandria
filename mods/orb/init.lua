3c27c315233e3f3aca161f9a23729d71447c320c
Phil Hagelberg <phil@hagelb.org>
2015-05-22 20:30:41 +0700

    Move utils to its own file.

--- mods/orb/init.lua
+++ mods/orb/init.lua
@@ -1,59 +1,19 @@
 -- a fake lil' OS
 
-orb = {}
-
--- utils
-
-orb.utils = {
-   split = function(str,div)
-      if(div=='') then return {str} end
-      if(div==str) then return {} end
-      local pos,res = 0,{}
-      for st,sp in function() return string.find(str,div,pos,true) end do
-         local str = string.sub(str,pos,st-1)
-         if(str ~= "") then table.insert(res,str) end
-         pos = sp + 1
-      end
-      table.insert(res,string.sub(str,pos))
-      return res
-   end,
-
-   shallow_copy = function(orig)
-      local orig_type = type(orig)
-      local copy
-      if orig_type == 'table' then
-         copy = {}
-         for orig_key, orig_value in pairs(orig) do
-            copy[orig_key] = orig_value
-         end
-      else -- number, string, boolean, etc
-         copy = orig
-      end
-      return copy
-   end,
-
+orb = {
    mod_dir = (minetest and minetest.get_modpath("orb")) or
       debug.getinfo(1,"S").source:sub(2, -9),
-
-   interp = function(s, tab)
-      return (s:gsub('($%b{})', function(w) return tab[w:sub(3, -2)] or w end))
-   end,
-
-   includes = function(tab, val)
-      for _,x in pairs(tab) do
-         if(x == val) then return true end
-      end
-   end
 }
 
-dofile(orb.utils.mod_dir .. "fs.lua")
-dofile(orb.utils.mod_dir .. "shell.lua")
+dofile(orb.mod_dir .. "utils.lua")
+dofile(orb.mod_dir .. "fs.lua")
+dofile(orb.mod_dir .. "shell.lua")
 
 -- interactively:
 if(arg) then
-   assert(orb.utils.mod_dir ~= "", "Could not determine mod dir.")
+   assert(orb.mod_dir ~= "", "Could not determine mod dir.")
    f = orb.fs.empty()
-   f1 = orb.fs.seed(orb.fs.proxy(f, "root"),
+   f1 = orb.fs.seed(orb.fs.proxy(f, "root", f),
                     {"technomancy", "buddyberg", "zacherson"})
    e0 = orb.shell.new_env("root")
    e1 = orb.shell.new_env("technomancy")
---
+++ mods/orb/utils.lua
@@ -0,0 +1,49 @@
+-- utils
+
+orb.utils = {
+   split = function(str,div)
+      if(div=='') then return {str} end
+      if(div==str) then return {} end
+      local pos,res = 0,{}
+      for st,sp in function() return string.find(str,div,pos,true) end do
+         local str = string.sub(str,pos,st-1)
+         if(str ~= "") then table.insert(res,str) end
+         pos = sp + 1
+      end
+      table.insert(res,string.sub(str,pos))
+      return res
+   end,
+
+   shallow_copy = function(orig)
+      local orig_type = type(orig)
+      local copy
+      if orig_type == 'table' then
+         copy = {}
+         for orig_key, orig_value in pairs(orig) do
+            copy[orig_key] = orig_value
+         end
+      else -- number, string, boolean, etc
+         copy = orig
+      end
+      return copy
+   end,
+
+   interp = function(s, tab)
+      return (s:gsub('($%b{})', function(w) return tab[w:sub(3, -2)] or w end))
+   end,
+
+   includes = function(tab, val)
+      for _,x in orb.utils.mtpairs(tab) do
+         if(x == val) then return true end
+      end
+   end,
+
+   mtpairs = function(tab)
+      local mt = getmetatable(tab)
+      if(mt) then
+         return mt.__iterator(tab)
+      else
+         return pairs(tab)
+      end
+   end,
+}
