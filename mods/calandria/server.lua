869b8ad8962c9948ffc4f736cdce6d4890b03eea
Phil Hagelberg <phil@hagelb.org>
2015-05-25 21:23:43 +0700

    Fixes #11, except that coroutine.yield() crashes.
    Other mods seem to be able to use coroutine.yield(), but when called
    here it crashes the server with "Attempt to yield across C-call boundary".

--- mods/calandria/server.lua
+++ mods/calandria/server.lua
@@ -1,9 +1,6 @@
--- can't use tables as table keys; lame but understandable
 local key_for = function(p) return "p" .. p.x .. "-" .. p.y .. "-" .. p.z end
 
 calandria.server = {
-   -- TODO: restarting the server clears this, so you have to place
-   -- server blocks anew for each server cycle
    placed = {},
 
    after_place = function(pos, placer, _itemstack, _pointed)
@@ -12,36 +9,78 @@ calandria.server = {
    end,
 
    make = function(player)
-      local f = orb.fs.empty()
-      orb.fs.seed(orb.fs.proxy(f, "root", f), {player})
-      local fs = orb.fs.proxy(f, player, f)
-      local envs = {}
-      local owner = player
-      envs[owner] = orb.shell.new_env(owner)
+      local f_root = orb.fs.empty()
+      orb.fs.seed(orb.fs.proxy(f_root, "root", f_root), {player})
+      local fs = orb.fs.proxy(f_root, player, f_root)
 
-      return {
-         exec = function(input, player)
-            if(not envs[player]) then return "No account on this server." end
+      return { fs = fs, sessions = {} }
+   end,
 
-            local out = {}
-            envs[player].write = function(x) table.insert(out, #out) end
-            orb.shell.exec(fs, envs[player], input)
-            return "$ " .. input .. "\n" .. table.concat(out, "\n") .. "\n\n"
-         end,
-      }
+   session = function(pos, server, player, channel)
+      local env = orb.shell.new_env(player)
+      local buffer = {}
+      env.write = function(output)
+         -- TODO: this crashes: attempt to yield across C-call boundary
+         -- coroutine.yield()
+         digiline:receptor_send(pos, digiline.rules.default, channel, output)
+      end
+      env.read = function()
+         -- without this, the smash shell will block forever, yaaaaay
+         -- while #buffer == 0 do coroutine.yield() end
+         return table.remove(buffer)
+      end
+      env.buffer_input = function(x)
+         table.insert(buffer, x)
+      end
+      server.sessions[player] = env
+      return env
    end,
 
    digiline_action = function(pos, node, channel, msg)
       if(type(msg) == "function") then
          local value = msg()
-         if not value.msg then return end
-         print("Executing "..value.msg)
+         local player = value.player
          local server = calandria.server.placed[key_for(pos)]
-         local result = server.exec(value.msg, value.player)
-         digiline:receptor_send(pos, digiline.rules.default, channel, result)
+         if not server then print("derp, no server") return end
+
+         if(value.code == "init") then
+            local env = calandria.server.session(pos, server, player, channel)
+            local fs = orb.fs.proxy(server.fs, player, server.fs)
+            orb.shell.exec(fs, env, "smash")
+         elseif value.msg and value.msg ~= "" then
+            print("Received: "..value.msg)
+            server.sessions[player].buffer_input(value.msg)
+         end
       end
    end,
+
+   path = minetest.get_worldpath() .. "/servers",
+
+   load = function()
+      local file = io.open(calandria.server.path, "r")
+      if(file) then
+         for k,v in pairs(minetest.deserialize(file:read("*all"))) do
+            placed[k] = { fs = v, sessions = {} }
+         end
+         file:close()
+      else
+         return {}
+      end
+   end,
+
+   save = function()
+      local file = io.open(calandria.server.path, "w")
+      local filesystems = {}
+      for k,v in pairs(placed) do
+         filesystems[k] = v.fs
+      end
+      file:write(minetest.serialize(filesystems))
+      file:close()
+   end,
 }
+
+placed = calandria.server.load()
+minetest.register_on_shutdown(calandria.server.save)
 
 minetest.register_node("calandria:server", {
                           description = "server",
@@ -68,4 +107,5 @@ minetest.register_node("calandria:server", {
                           },
                           after_place_node = calandria.server.after_place
                           -- TODO: remove on destruct
+                          -- TODO: digiterms need to send destruct messages too
 })
