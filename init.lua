78ab0add8dad86458c9ce44a1b016e4e70caae62
Phil Hagelberg <phil@hagelb.org>
2015-06-22 21:22:04 +0700

    Don't lower gravity.

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
-minetest.register_on_joinplayer(function(player)
-      player:set_physics_override({gravity = 0.2})
-end)
-
 calandria = calandria or {}
 
 dofile(minetest.get_modpath("calandria").."/nodes.lua")
