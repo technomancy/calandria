8f12faefaab83eaabb78a08141732c032fb8b037
Phil Hagelberg <phil@hagelb.org>
2015-06-27 13:40:04 +0700

    Removed unused nodes and textures.

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 calandria = calandria or {}
 
-dofile(minetest.get_modpath("calandria").."/nodes.lua")
 dofile(minetest.get_modpath("calandria").."/server.lua")
 dofile(minetest.get_modpath("calandria").."/terminal.lua")
 dofile(minetest.get_modpath("calandria").."/editor.lua")
